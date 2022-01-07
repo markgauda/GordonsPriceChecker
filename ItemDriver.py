345a342d87c2087ad750bcfec7519f1db26ff1d0
markgauda <markgaud@buffalo.edu>
2022-01-07 16:06:37 -0500

    Changed to match new features

--- ItemDriver.py
+++ ItemDriver.py
@@ -10,8 +10,10 @@ def main():
     testItem = Item.GordonsItem(477970, 100)
     assert testItem.price_wanted == 100, "The price wanted was not set to 100"
     assert testItem.id == 477970, "The item ID was not set to 477970"
-    assert testItem.price_now == None, "The item price now was not set to None"
-    testItem.update_price_now()
-    assert testItem.price_now == 93.99, "The item price was not succesfuly updated"
+    assert testItem.price_now == 0.0, "The item price now was not set to 0.0"
+    assert testItem.quantity_in_stock == 0, "The quantity was not set to 0"
+    testItem.update_item()
+    assert testItem.price_now == 93.99, "The item price was not succesfuly updated (check website to make sure it didn't change)"
+    assert testItem.quantity_in_stock == 5, "The quantity was not succesfuly updated (check website to make sure it didn't change)"
 
 main()
