2dafdfc5ac98d3eb9469fbe13e21fa0b800ce156
markgauda <markgaud@buffalo.edu>
2022-01-09 16:34:30 -0500

    mostly debugged, it can send emails!

--- EmailHandler.py
+++ EmailHandler.py
@@ -10,8 +10,10 @@ logging.basicConfig(level = logging.DEBUG, format = loggingFormat)
 class EmailHandler():
     def __init__(self):
         self.login_file = "login_file.txt" #this is where your login info is stored
-        self.get_login_from_file() #Get your email and password from that file
-        self.contacts_file = "contacts_list.txt"
+        credentials = self.get_login_from_file() #Get your email and password from that file
+        self.email_addr = credentials[0]
+        self.email_pass = credentials[1]
+        self.contacts_file = "contact_list.txt"
         self.contacts = self.get_contacts()
         self.smtp_connection = self.connect_to_smtp_server()
         self.connected_to_server = self.start_tls_connection()
@@ -23,8 +25,9 @@ class EmailHandler():
         the second line
         """
         with open(self.login_file, "rt") as input_file:
-            self.email_addr = input_file.readline
-            self.email_pass = input_file.readline
+            email_addr = input_file.readline()[0:-1] #get rid of the \n
+            email_pass = input_file.readline()
+        return (email_addr, email_pass)
 
     def get_contacts(self):
         """This function will look in the contatcts_list file and return a list of
@@ -64,6 +67,7 @@ class EmailHandler():
             log_message = "Starting the TLS connection to SMTP server"
             logging.debug(log_message)
             #send hello message
+            
             hello_connection = self.smtp_connection.ehlo()
             if (hello_connection[0] != 250):
                 log_message = "The SMTP greeting failed with code "+\
@@ -72,6 +76,7 @@ class EmailHandler():
                 return False
             
             #Start TLS encryption
+            
             server_status = self.smtp_connection.starttls()
             if (server_status[0] != 220):
                 log_message = "The server could not establish a TLS connection, "+\
@@ -80,12 +85,17 @@ class EmailHandler():
                 return False
             
             #log in to the server
-            loging_status = self.smtp_connection.login(self.email_addr, self.email_pass)
-            if (loging_status[0] != 235):
-                log_message = "The server could not accept your email address or password, "+\
-                    "the error code is: " + loging_status[0]
-                logging.info(log_message)
-                return False
+            try:
+                loging_status = self.smtp_connection.login(self.email_addr, self.email_pass)
+                if (loging_status[0] != 235):
+                    log_message = "The server could not accept your email address or password, "+\
+                        "the error code is: " + loging_status[0]
+                    logging.info(log_message)
+                    return False
+            except smtplib.SMTPAuthenticationError:
+                log_message = "The login to the SMTP server failed. The email or password could have been"+\
+                    " wrong, or make sure that google security allows 'less secure apps'"
+                logging.critical(log_message)
             
             log_message = "Successfuly connected and logged into SMTP server"
             logging.debug(log_message)
@@ -104,8 +114,12 @@ class EmailHandler():
             subject (String): This is the subject of the email
             message (String): This is the email you are going to send out
         """
+        log_message = "Starting to send an email"
+        logging.debug(log_message)
         if (not subject.endswith("\n")):
             subject = subject + "\n"
+        if (not subject.startswith("Subject:")):
+            subject = "Subject: " + subject
         for recipient_email in self.contacts:
             try:
                 self.smtp_connection.sendmail(self.email_addr, recipient_email, (subject + message))
