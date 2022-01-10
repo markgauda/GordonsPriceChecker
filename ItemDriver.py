716466bc81dcb1a00d0d473aaaaceec7f794be11
markgauda <markgaud@buffalo.edu>
2022-01-10 17:08:28 -0500

    updated tests to match object

--- ItemDriver.py
+++ ItemDriver.py
@@ -13,7 +13,8 @@ def main():
     assert testItem.price_now == 0.0, "The item price now was not set to 0.0"
     assert testItem.quantity_in_stock == 0, "The quantity was not set to 0"
     testItem.update_item()
-    assert testItem.price_now == 93.99, "The item price was not succesfuly updated (check website to make sure it didn't change)"
-    assert testItem.quantity_in_stock == 5, "The quantity was not succesfuly updated (check website to make sure it didn't change)"
+    assert testItem.price_now == 94.99, "The item price was not succesfuly updated (check website to make sure it didn't change)"
+    assert testItem.quantity_in_stock == 3, "The quantity was not succesfuly updated (check website to make sure it didn't change)"
+    testItem.send_notification_email()
 
 main()
