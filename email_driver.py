b3bb422e52c69f62513e5e4f7a4bfa8105890e2c
markgauda <markgaud@buffalo.edu>
2022-01-10 13:40:13 -0500

    added better documentation

--- email_driver.py
+++ email_driver.py
@@ -1,5 +1,5 @@
 """This file was created by Mark Gauda in the winter of 2022
-This file is a test driver for the email.py module
+This file is a test driver for the EmailHandler.py module
 """
 import EmailHandler
 
@@ -7,4 +7,6 @@ email_object = EmailHandler.EmailHandler()
 
 print(email_object.connected_to_server)
 
-email_object.send_an_email("This is a test", "Hello from the program!")
+subject = "This is a test"
+message = "Hello from the program!"
+email_object.send_an_email(subject, message)
